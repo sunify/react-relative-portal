7912e997e7f2c365bf1fbd4436589c343787d8f1
Sergey Andreev <s.andreev@magdv.com>
2018-04-09 13:50:33 +0700

    fix issue #7, rewrite on react protal native

--- src/Portal.js
+++ src/Portal.js
@@ -14,68 +14,44 @@ export default class Portal extends React.Component {
 
     if (canUseDOM) {
       this.node = document.createElement('div');
-      document.body.appendChild(this.node);
-
       this.root = null;
       this.handleRootRef = (root) => {
-        if (root !== this.root) {
-          if (this.root) {
-            this.root.removeEventListener('click', this.handleInClick);
-          }
-          if (root) {
-            root.addEventListener('click', this.handleInClick);
-          }
-        }
         this.root = root;
       };
 
-      // The previous implementation triggered `onOutClick` after a click in the `Portal` content
-      // if it gets re-rendered during that click. It assumed that if the clicked element
-      // is not found in the root element via `root.contains(e.target)`, it's outside.
-      // But if after re-render the clicked element gets removed from the DOM, so it cannot be found
-      // in the root element. Instead we capture and flag the click event before it bubbles up
-      // to the `document` to be handled by `handleOutClick`.
-      this.isInClick = false;
-      this.handleInClick = () => {
-        this.isInClick = true;
-      };
-
       this.handleOutClick = (e) => {
-        const isOutClick = !this.isInClick;
-        this.isInClick = false;
-
         const { onOutClick } = this.props;
-        if (isOutClick && typeof onOutClick === 'function') {
-          onOutClick(e);
+        if (typeof onOutClick === 'function') {
+          if (this.root && !this.root.contains(e.target)) {
+            onOutClick(e);
+          }
+          if (!this.root) {
+            onOutClick(e);
+          }
         }
       };
 
       document.addEventListener('click', this.handleOutClick, true);
     }
   }
-
-  componentWillUpdate({ onOutClick, ...props }) {  // eslint-disable-line no-unused-vars
-    // It's recommended to use `ref` callbacks instead of `findDOMNode`. https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-find-dom-node.md
-    ReactDOM.unstable_renderSubtreeIntoContainer(
-      this,
-      <div {...props} ref={this.handleRootRef} />,
-      this.node
-    );
+  componentDidMount() {
+    if (canUseDOM) {
+      document.body.appendChild(this.node);
+    }
   }
 
   componentWillUnmount() {
     if (canUseDOM) {
-      // `this.handleRootRef` won't be called with `null`, so cleanup here.
-      if (this.root) {
-        this.root.removeEventListener('click', this.handleInClick);
-      }
-      document.removeEventListener('click', this.handleOutClick);
+      document.removeEventListener('click', this.handleOutClick, true);
       document.body.removeChild(this.node);
     }
   }
 
   render() {
-    return null;
+    return ReactDOM.createPortal(
+      <div {...this.props} ref={this.handleRootRef}>{this.props.children}</div>,
+      this.node,
+    );
   }
 
 }
